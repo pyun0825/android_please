064f2235239d81b00337269b83a367d84c841500
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-07-01 14:40:27 +0900

    adding list view

---
+++ app/src/main/java/com/example/android_please/fragments/ThirdFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android_please.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android_please.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+}
